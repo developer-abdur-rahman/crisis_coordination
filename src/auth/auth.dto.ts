6851b05858a21f97262c0ec6e6374507f26735fc
developer-abdur-rahman <dev.abdurrahman@proton.me>
2026-06-09 15:08:06 +0600

    Register user DTO

---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export enum registerUserRole {
+  USER,
+  VOLUNTEER,
+}
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(40)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsEnum(registerUserRole)
+  role: registerUserRole;
+}
